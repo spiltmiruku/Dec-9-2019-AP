130a2e2fdedd464bcc525184e41c7ca5bb126920
spiltmiruku <spiltmiruku@gmail.com>
2019-12-09 16:33:33 -0700

    Finished 1-3 from 'group by'

--- sql-two-ap.sql
+++ sql-two-ap.sql
@@ -80,4 +80,23 @@ WHERE genre_id =
 AND composer IS null;
 
 
+SELECT COUNT (*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+GROUP BY g.name;
+
+SELECT COUNT (*), g.name
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+WHERE g.name = 'Pop' OR g.name = 'Rock'
+GROUP BY g.name;
+
+SELECT a.name, COUNT (*)
+FROM album al
+JOIN artist a ON a.artist_id = al.artist_id
+GROUP BY a.name;
+
+
+
+
 
