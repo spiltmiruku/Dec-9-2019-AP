48569160ef401c2f9a4f2af8c045732049620a46
spiltmiruku <spiltmiruku@gmail.com>
2019-12-09 14:50:30 -0700

    Finished 'practice joins' 1-8 not including black diamond

--- sql-two-ap.sql
+++ sql-two-ap.sql
@@ -17,3 +17,16 @@ SELECT pt.track_id FROM playlist_track pt
 JOIN playlist  p ON p.playlist_id = pt.playlist_id
 WHERE p.name = 'Music';
 
+SELECT t.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+WHERE pt.playlist_id = 5;
+
+SELECT t.name, p.name FROM track t
+JOIN playlist_track pt ON pt.track_id = t.track_id
+JOIN playlist p ON p.playlist_id = pt.playlist_id;
+
+SELECT t.name, a.title FROM track t
+JOIN album a ON a.album_id = t.album_id
+JOIN genre g ON g.genre_id = t.genre_id
+WHERE g.name = 'Alternative & Punk';
+
