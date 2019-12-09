fec510b270dd6981ebe08cbc25ea4f2fd3542d44
spiltmiruku <spiltmiruku@gmail.com>
2019-12-09 15:23:58 -0700

    Completed 1-3 in 'practice nested queries'

--- sql-two-ap.sql
+++ sql-two-ap.sql
@@ -30,3 +30,24 @@ JOIN album a ON a.album_id = t.album_id
 JOIN genre g ON g.genre_id = t.genre_id
 WHERE g.name = 'Alternative & Punk';
 
+
+SELECT * FROM invoice
+WHERE invoice_id IN 
+	( SELECT invoice_id FROM invoice_line WHERE unit_price > 0.99);
+
+SELECT * FROM playlist_track
+WHERE playlist_id IN
+ 	( SELECT playlist_id FROM playlist WHERE name = 'Music');
+
+SELECT name FROM track
+ WHERE track_id IN
+ 	( SELECT track_id FROM playlist_track WHERE playlist_id = 5);
+ 
+
+
+
+
+
+
+
+
